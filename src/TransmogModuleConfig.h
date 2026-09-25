16d542c0d56817a6d154ae1a3451ba030fac8a0c
Chron-cmangos <chrongh@proton.me>
2026-09-25 14:07:03 +0200

    Added mixed armor and weapon types options.
    Accompanied by changes done to:
    - transmog.conf
    - TransmogModule.h
    - TransmogModule.cpp
    - TransmogModuleConfig.cpp
    
    Cherry picked from AzerothCore transmog module with help of AI

--- src/TransmogModuleConfig.h
+++ src/TransmogModuleConfig.h
@@ -16,5 +16,14 @@ namespace cmangos_module
         bool tokenRequired;
         uint32 tokenEntry;
         uint32 tokenAmount;
+
+        // Mixed Armor Options
+        bool allowMixedArmorTypes;
+        bool allowLowerTiers;
+        bool allowChestRobeMismatch;
+
+        // Mixed Weapon Options
+        uint8 allowMixedWeaponTypes;
+        bool allowMixedWeaponHandedness;
     };
 }
